493ddd3840f914d05c1efffa452d7ccb5aee42a0
Zhorod <45471037+Zhorod@users.noreply.github.com>
2022-10-15 19:54:45 +0100

    Added file saving to security camera.
    Added file saving code to security camera project.

--- securitycamera/securitycamera.py
+++ securitycamera/securitycamera.py
@@ -2,23 +2,34 @@
 # 15th October 2022
 
 import RPi.GPIO as GPIO
+import time
+from picamera import PiCamera
+from time import sleep
 
 ledPin = 12 # define ledPin
 sensorPin = 11 # define sensorPin
 
+camera = PiCamera()
+sleep(2)
+print('Initialised camera')
+
 def setup():
-	GPIO.setmode(GPIO.BOARD) # use PHYSICAL GPIO Numbering
-	GPIO.setup(ledPin, GPIO.OUT) # set ledPin to OUTPUT mode
-	GPIO.setup(sensorPin, GPIO.IN) # set sensorPin to INPUT mode
+    GPIO.setmode(GPIO.BOARD) # use PHYSICAL GPIO Numbering
+    GPIO.setup(ledPin, GPIO.OUT) # set ledPin to OUTPUT mode
+    GPIO.setup(sensorPin, GPIO.IN) # set sensorPin to INPUT mode
 
 def loop():
-	while True:
-		if GPIO.input(sensorPin)==GPIO.HIGH:
-			GPIO.output(ledPin,GPIO.HIGH) # turn on led
-			print ('led turned on >>>')
-		else :
-			GPIO.output(ledPin,GPIO.LOW) # turn off led
-			print ('led turned off <<<')
+    while True:
+        if GPIO.input(sensorPin)==GPIO.HIGH:
+            # we have detected some movement
+            # turn on the LED and take a picture
+            GPIO.output(ledPin,GPIO.HIGH) # turn on led
+            now = time.localtime(time.time())
+            camera.capture('/home/pi/Desktop/Images/image '+time.strftime("%c", now)+'.jpg')
+            
+            print ('captured images')
+        else:
+            GPIO.output(ledPin,GPIO.LOW) # turn off led
 
 def destroy():
 	GPIO.cleanup() # Release GPIO resource
